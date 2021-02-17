036648475cef495a9287e8935b3d0cdeef29502e
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-02-17 10:50:17 -0500

    Adjust size of covariate plots, remove unused code

--- app/modules/covariates.R
+++ app/modules/covariates.R
@@ -1,6 +1,7 @@
 covariates_ui <- function(id, betas) {
   ns <- shiny::NS(id)
 
+  # Function to create covariate selectors
   covariate_input <- function(label, i, selected = NULL) {
     selectizeInput(
       multiple = TRUE,
@@ -46,8 +47,11 @@ covariates_ui <- function(id, betas) {
       hr(),
 
       # Show a plot of the generated distribution
-      withSpinner(plotOutput(ns("BetaPlot"))),
-      plotOutput(ns("BetaPlotLegend"), inline = TRUE)
+      column(12,
+        align = "center",
+        withSpinner(plotOutput(ns("BetaPlot"), width = "80%", height = "500px")),
+      plotOutput(ns("BetaPlotLegend"), height = "100px")
+      )
     )
   )
 }
@@ -58,33 +62,37 @@ covariates_server <- function(id, betas) {
     function(input, output, session) {
 
       # coefficients
-      output$BetaPlot <- renderPlot({
-        vars_selected <- c(
-          input$covariate_1, input$covariate_2,
-          input$covariate_3, input$covariate_4,
-          input$covariate_5, input$covariate_6,
-          input$covariate_7, input$covariate_8
-        )
-        p <- betas %>%
-          filter(
-            ci_width == 0.5,
-            indicator == input$indicator,
-            race == "Total",
-            variable_description %in% vars_selected, year < 2019
-          ) %>%
-          ggplot(aes(year, value, color = variable_description)) +
-          geom_point() +
-          ylab("coefficient") +
-          theme_bw(base_size = 14) +
-          scale_x_continuous(breaks = seq(2003, 2018, by = 4)) +
-          geom_errorbar(aes(ymin = lower, ymax = upper), width = 0) +
-          facet_wrap(~division) +
-          scale_color_brewer(name = "covariate", palette = "Set1") +
-          theme(legend.position = "bottom", legend.direction = "vertical")
-        p_leg <- get_legend(p)
-        p + theme(legend.position = "none")
-      }) # , height = 500, width = 700
+      output$BetaPlot <- renderPlot(
+        res = 96,
+        {
+          vars_selected <- c(
+            input$covariate_1, input$covariate_2,
+            input$covariate_3, input$covariate_4,
+            input$covariate_5, input$covariate_6,
+            input$covariate_7, input$covariate_8
+          )
+          p <- betas %>%
+            filter(
+              ci_width == 0.5,
+              indicator == input$indicator,
+              race == "Total",
+              variable_description %in% vars_selected, year < 2019
+            ) %>%
+            ggplot(aes(year, value, color = variable_description)) +
+            geom_point() +
+            ylab("coefficient") +
+            theme_bw(base_size = 14) +
+            scale_x_continuous(breaks = seq(2003, 2018, by = 4)) +
+            geom_errorbar(aes(ymin = lower, ymax = upper), width = 0) +
+            facet_wrap(~division) +
+            scale_color_brewer(name = "covariate", palette = "Set1") +
+            theme(legend.position = "bottom", legend.direction = "vertical")
+          p_leg <- get_legend(p)
+          p + theme(legend.position = "none")
+        }
+      )
       output$BetaPlotLegend <- renderPlot(
+        res = 96,
         {
           vars_selected <- c(
             input$covariate_1, input$covariate_2,
@@ -105,10 +113,9 @@ covariates_server <- function(id, betas) {
             theme(legend.position = "bottom", legend.direction = "vertical")
           p_leg <- get_legend(p)
           as_ggplot(p_leg)
-        },
-        height = 200,
-        width = 400
+        }
       )
+
       output$BetaTable <- renderTable(betas %>%
         group_by(division, variable_description) %>%
         summarise(mean = mean(value)) %>%
@@ -119,46 +126,6 @@ covariates_server <- function(id, betas) {
           "covariate" = variable_description,
           "average effect" = mean
         ))
-
-      # scatter
-      output$ScatterPlot <- renderPlot({
-        covar <- betas %>%
-          filter(variable_description == input$scatter) %>%
-          select(covariate_name) %>%
-          slice(1) %>%
-          pull()
-        if (input$groups == TRUE) {
-          p <- d_data %>%
-            rename(this_c = covar) %>%
-            ggplot(aes(this_c, ent_pc, color = division)) +
-            geom_point() +
-            scale_y_log10() +
-            scale_x_log10() +
-            ylab("entries per 1,000") +
-            xlab(input$scatter) +
-            theme_bw(base_size = 14) +
-            scale_color_viridis_d() +
-            ggtitle(paste("Entries per capita versus", input$scatter))
-        }
-        else {
-          p <- d_data %>%
-            rename(this_c = covar) %>%
-            ggplot(aes(this_c, ent_pc)) +
-            geom_point() +
-            scale_y_log10() +
-            scale_x_log10() +
-            ylab("entries per 1,000") +
-            xlab(input$scatter) +
-            theme_bw(base_size = 14) +
-            ggtitle(paste0("Entries per capita versus \n", input$scatter))
-        }
-        if (input$fitted_line == TRUE) {
-          p + geom_smooth(method = "lm")
-        }
-        else {
-          p
-        }
-      })
     }
   )
 }
